0517ff0939f1407bd88b8ce4e00ad6138db9c3a3
azril335 <syaifulazril89@gmail.com>
2025-10-24 13:54:31 +0700

    commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random rand = new Random();
+
+        while (true) {
+            System.out.println("Welcome To My Game");
+            System.out.println("===== Game Tebak Angka =====");
+            System.out.print("Pilih angka 1-10 : ");
+
+            int random = rand.nextInt(10) + 1; // menghasilkan angka 1–10
+            int player = input.nextInt();
+
+            if (player == random) {
+                System.out.println("You win!");
+                System.out.println("Angkanya adalah " + random);
+                break;
+            } else {
+                System.out.println("You lose!");
+                System.out.println("Angkanya adalah " + random);
+            }
+
+            System.out.println();
+        }
+
+        input.close();
+    }
+}
